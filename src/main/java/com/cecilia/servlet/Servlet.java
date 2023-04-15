a7646f956175749b69d9df2bbd0ed5784d2a5ae3
CeciliaEscobar <escobarceciramoa@gmail.com>
2023-04-15 16:48:49 -0400

    Servlet DoPost Parameter JSP

--- src/main/java/com/cecilia/servlet/Servlet.java
+++ src/main/java/com/cecilia/servlet/Servlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -17,21 +19,23 @@ import java.io.IOException;
  */
 @WebServlet(urlPatterns= "/inicio") //localhost:8080/inicio
 public class Servlet extends HttpServlet {
+    
+    private List<String> lista = new ArrayList<>();
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
-        
-       /* String param = req.getParameter ("nombre");
-        if(param == null){
-            resp.getWriter().append("Hola, como estas?");
-        }else{
-            String saludo = "Hola," + param + "!";
-            resp.getWriter().append(saludo);
-        }*/
-       
-       req.setAttribute("name", "Cecilia");
+     
+       req.setAttribute("nomina" , lista);
         
        req.getServletContext().getRequestDispatcher("/paginainicio.jsp").forward(req,resp);
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      String nombre = req.getParameter("nombre");
+      this.lista.add(nombre);
+      resp.sendRedirect(req.getContextPath() + "/inicio");
     }
 
 }
